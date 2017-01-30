b26e44f7d66e79e1ec24f8856706b766ac33a3a7
bgb0923 <bgb0923@gmail.com>
2017-01-30 18:23:18 -0500

    Answered Questions

--- ArrayMethods.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ ArrayMethods.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,105 +14,75 @@
 /*: question1
  ### 1. Create a changeable list for the five days of the week called 'daysOfTheWeek' and print each one by using a loop.
  */
-// write your code here
-
-
-
-
-
-
-
+var DOTW = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+for day in DOTW {
+    print(day)
+}
 /*: question2
  ### 2. Create an unchanging list for the five days of the week called 'numDaysOfTheWeek' and print each one by using a loop, prefixed by the position of the day of the week.
  */
-// write your code here
-
-
-
-
-
-
+print("")
+let numDOTW = [1,2,3,4,5]
+for number in numDOTW {
+    print("\(number)) \(DOTW[number - 1])")
+}
 
 /*: question3
  ### 3. Create an empty array of strings called 'emptyArray' and check to see if it's empty, printing appropriate messages.
  */
-// write your code here
-
-
-
-
-
-
-
-
+print("")
+//var emptyArray : [String] I kept getting an error because i'm checknig if its empty before its initialized.  Wasn't sure how to solve for that.
+var emptyArray = ["Test"]
+emptyArray.remove(at: 0)
+if emptyArray.isEmpty {
+    print("Array is Empty")
+} else {
+    print("The Array is Not Empty")
+}
 /*: question4
  ### 4.  Create an empty array of strings called 'reverseEmptyArray' and check to see if it's empty, printing appropriate messages. Use reversed logic from question 3.
  */
-// write your code here
-
-
-
-
-
-
-
-
-
+// I don't know what you're asking here
 /*: question5
  ### 5. Find out the size of the 'daysOfTheWeek' array created earlier, and print it to the screen.
  */
-// write your code here
-
-
-
-
-
-
-
-
+print("")
+print(DOTW.count)
 /*: question6
  ### 6. Add the two days of the weekend to the 'daysOfTheWeek array and create an enumerated loop to print the values.
  */
-// write your code here
-
-
-
-
-
-
-
+print("")
+DOTW.append("Saturday")
+DOTW.append("Sunday")
+for day in DOTW {
+    print(day)
+}
 /*: question7
  ### 7. Some people start the week on a Sunday. Remove Sunday from the end of the 'daysOfTheWeek' array and then re-add it to the beginning of the array.
  */
-// write your code here
-
-
-
-
-
-
-
-
+print("")
+DOTW.remove(at: 6)
+DOTW.insert("Sunday", at: 0)
+for day in DOTW {
+    print(day)
+}
 /*: question8
  ### 8. Loop through the 'daysofTheWeek' array and change each value to lower case.
  */
-// write your code here
-
-
-
-
-
-
-
+print("")
+for day in DOTW {
+    print(day.lowercased())
+}
 
 /*: question9
  ### 9. Check to see if the size of the array is greater than 5, i.e. contains the days of the weekend. If so, remove the days of the weekend from the array.
  */
-// write your code here
-
-
-
-
-
-
+if DOTW.count > 5 {
+    DOTW.remove(at: 6)
+    DOTW.remove(at: 0)
+}
+print("")
+for day in DOTW {
+    print(day)
+}
 //: Check here on the solution branch for a link to the solutions
